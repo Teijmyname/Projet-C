1bf7c23e019860f4ddcaf371cd516723547fa97e
Teij_jayantilal <101254615+Teijmyname@users.noreply.github.com>
2022-10-24 18:02:22 +0200

    Add files via upload

---
+++ PROJETC/node.c
@@ -0,0 +1,5 @@
+//
+// Created by Teij Jayantilal on 24/10/2022.
+//
+
+#include "node.h"
---
+++ PROJETC/node.h
@@ -0,0 +1,8 @@
+//
+// Created by Teij Jayantilal on 24/10/2022.
+//
+
+#ifndef PROJETC_NODE_H
+#define PROJETC_NODE_H
+
+#endif //PROJETC_NODE_H
