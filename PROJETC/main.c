f066a1f7a15b36eb66157395a2e27356f830d67d
Romain SAMSON <romain.samson@efrei.net>
2022-10-31 19:59:48 +0100

    accéder à un fichier et l'afficher

--- PROJETC/main.c
+++ PROJETC/main.c
@@ -1,6 +1,20 @@
-#include <stdio.h>
+
+#include "node.h"
+#define ENTREE "C:\\Users\\Romain SAMSON\\Desktop\\Projet-C\\PROJETC\\dico_10_lignes.txt"
+#define TAILLE_MAX 80
 
 int main() {
-    printf("Hello, World!\n");
+    FILE * inputFile;
+    char ligne[TAILLE_MAX];
+    inputFile = fopen (ENTREE, "r");
+    if (inputFile == NULL){
+        printf("erreur d'ouverture de fichier");
+    }
+    while(fgets(ligne, TAILLE_MAX, inputFile)!= NULL){
+        printf("%s", ligne);
+    }
+
+    fclose(inputFile);
     return 0;
 }
+
--- PROJETC/node.h
+++ PROJETC/node.h
@@ -1,8 +1,14 @@
 //
 // Created by Teij Jayantilal on 24/10/2022.
-//
-
+#include <stdio.h>
+#include <stdlib.h>
 #ifndef PROJETC_NODE_H
 #define PROJETC_NODE_H
-
+//Structure de tableaux pour les nœuds
+#define ALPHABET_SIZE 26
+typedef struct s_letter_node
+{
+char letter;
+struct s_letter_node * sons [ALPHABET_SIZE] ;
+} t_letter_node, *p_letter_node ;
 #endif //PROJETC_NODE_H
